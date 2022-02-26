0d979e2e8a8efd9ecf93e595204e732c531cbf9d
seojin0818 <100174690+seojin0818@users.noreply.github.com>
2022-02-26 17:21:32 +0900

    Update Test04.java

--- start/src/day04/Test04.java
+++ start/src/day04/Test04.java
@@ -9,21 +9,31 @@ package day04;
  */
 
 import java.util.*;
+// 이렇게 사용할 클래스의 경로를 지정하면 java.util 패키지 하위에 바로 속한 클래스만 사용할 준비를 하게 됨
 
 public class Test04 {
 	public static void main(String[] args) {
+	// 진입점 함수
+		
 		// 할 일
-		// 입력 도구 준비
+		// 1) 입력 도구 준비
 		Scanner sc = new Scanner(System.in);
-		// 입력 메세지 출력
+		
+		// 2) 입력 받기 전에 입력 메세지 출력
 		System.out.print("정수 입력 : ");
-		// 입력 받아서 변수에 기억시키고
+		// System.out.print(내용);
+		// 내용 출력 + 줄 바꿈 x
+		
+		// 3) 입력 받아서 변수에 기억 시킨다.
 		int num = sc.nextInt();
-		// 짝수인지 홀수인지 판별하고
+		
+		// 4) 짝수인지 홀수인지 판별한다.
 		String result = (num % 2 == 0) ? ("짝수") : ("홀수");
+		// () ? () : ();
 		
-		// 출력하고
-		System.out.println("입력 받은 숫자 [ " + num + " ] 은 ' " + result + " ' 입니다.");
-		
+		// 5) 출력한다.
+		System.out.println("입력 받은 숫자 [ " + num + " ] 은 ' " + result +  " ' 입니다.");
+		// System.out.println(내용);
+		// 내용 출력 + 줄 바꿈
 	}
 }
