a954ca2a52483ef679170369aaacb3c4865205d5
seojin0818 <100174690+seojin0818@users.noreply.github.com>
2022-02-26 13:21:56 +0900

    Update Test01.java

--- start/src/day03/Test01.java
+++ start/src/day03/Test01.java
@@ -2,8 +2,13 @@ package day03;
 
 public class Test01 {
   public static void main(String[] args){
+  // 진입점 함수
+    
     System.out.println("***Git 연동 성공***");
-    System.out.println("깃 다시 성공~~~~~!");
+    System.out.println("Git 다시 성공~~~~~!");
+    
+    // System.out.println(내용);
+    // 내용 출력 + 줄 바꿈
     
   }
 }
