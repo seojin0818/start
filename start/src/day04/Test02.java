a504723a572a888808b4603de387a188546b2e6f
seojin0818 <100174690+seojin0818@users.noreply.github.com>
2022-02-26 17:03:07 +0900

    Update Test02.java

--- start/src/day04/Test02.java
+++ start/src/day04/Test02.java
@@ -2,10 +2,17 @@ package day04;
 
 public class Test02 {
 	public static void main(String[] args) {
+	// 진입점 함수
+		
 		int no1 = 20;
 		int no2 = 36;
 		
-		System.out.println(no1 & no2); // 이 때 &는 논리연산자가 아니라 비트연산자로 사용
+		System.out.println(no1 & no2); 
+		// 이 때 &는 논리연산자가 아니라 비트연산자로 사용
+		// 비트연산자 : 피연산자들을 내부적으로 bit로 변경한 뒤 연산 수행
+		// System.out.println(내용);
+		// 내용 출력 + 줄 바꿈
 		
+		// ==> 4
 	}
 }
