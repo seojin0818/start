8b5f4ecd7ab0b8e082e455852a1aacd035686895
seojin0818 <100174690+seojin0818@users.noreply.github.com>
2022-02-26 13:46:34 +0900

    Update Test04.java

--- start/src/day03/Test04.java
+++ start/src/day03/Test04.java
@@ -2,16 +2,23 @@ package day03;
 
 public class Test04 {
 	public static void main(String[] args) {
+	// 진입점 함수
+		
 		int no1 = 5;
 		int no2 = 3;
-		
-		System.out.println("no1 / no2 = " + (no1 / no2));
+		// 같은 영역 안에서 변수의 이름은 같을 수 없음
 		
 		System.out.println("no1 + no2 = " + (no1 + no2));
 		System.out.println("no1 - no2 = " + (no1 - no2));
 		System.out.println("no1 * no2 = " + (no1 * no2));
 		System.out.println("no1 / no2 = " + (no1 / no2));
 		System.out.println("no1 % no2 = " + (no1 % no2));
+		
+		// System.out.println(내용);
+		// 내용 출력 + 줄 바꿈
+		
+		// 산술 연산자 : 사칙연산자(+, -, *, /) 나머지 값을 구하는 연산자(%)
+		
 		
 		
 		
