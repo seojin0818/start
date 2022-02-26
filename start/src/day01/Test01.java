fbe278de79d3e7d2100730a3a46495dd65e064ac
seojin0818 <100174690+seojin0818@users.noreply.github.com>
2022-02-26 13:18:52 +0900

    Update Test01.java

--- start/src/day01/Test01.java
+++ start/src/day01/Test01.java
@@ -2,6 +2,8 @@ package day01;
 
 public class Test01 {
 	public static void main(String[] args) {
+	// 진입점 함수
+		
 		System.out.println("첫번째 자바 클래스");
 		// System.out.println(내용);
 		// 내용 출력 + 줄 바꿈
