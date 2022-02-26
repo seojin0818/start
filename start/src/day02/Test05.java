af6b5ac0560a4c8e1db8489defecf3c4a8fbc1e3
seojin0818 <100174690+seojin0818@users.noreply.github.com>
2022-02-26 13:05:09 +0900

    Update Test05.java

--- start/src/day02/Test05.java
+++ start/src/day02/Test05.java
@@ -2,11 +2,18 @@ package day02;
 
 public class Test05 {
 	public static void main(String[] args) {
-		// 3.14를 float 타입 변수 pi에 기억해보자.
+	// 진입점 함수
 		
-		float pi ; //변수 선언
+		// 문제 ] 3.14를 float 타입 변수 pi에 기억해보자.
 		
-		pi = 3.14f;
+		// 할 일
+		// 1) 3.14는 소수점이 있는 실수형이므로 float, double 둘 다 가능하지만 실수형은 리터럴 영역에 double형으로 자동 저장된다.
+		// 2) 그런데 문제에서는 float 타입을 사용해야 하므로 형변환을 시켜줘야한다.
+		// 3) 리터럴 형변환을 이용한다. (데이터f; -> float타입)
+		
+		float pi ; // 변수 선언
+		
+		pi = 3.14f; // 리터럴 형변환
 	}
 	
 	
