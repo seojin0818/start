b793035a4722e7fbf1114b11caa3a5763ebedfa8
User <User@DESKTOP-RT0MR1A>
2022-03-20 15:36:03 +0900

    day15 commit

--- start/src/day15/Ex03.java
+++ start/src/day15/Ex03.java
@@ -1,29 +1,49 @@
 package day15;
 
-/*
+	/*
 
-학생 한명의 정보를 저장할 클래스를 제작하세요.
+	학생 한명의 정보를 저장할 클래스를 제작하세요.
 
-정보는
-	학생이름, 반, 번호, 
-	자바, 데이터베이스, 웹, 스프링 점수
-	총점, 평균
-를 기억하게 하고
+	정보는
+		학생이름, 반, 번호, 
+		자바, 데이터베이스, 웹, 스프링 점수
+		총점, 평균
+	를 기억하게 하고
 
-10명의 학생을 TreeSet에 입력하고
-반 순서대로 정렬되도록 하세요.
-반이 같으면 번호순으로 정렬되도록 하세요.
+	10명의 학생을 TreeSet에 입력하고
+	반 순서대로 정렬되도록 하세요.
+	반이 같으면 번호순으로 정렬되도록 하세요.
 
-*/
+	*/
 
-public class Ex03 {
+	import java.util.*;
 
-public Ex03() {
-	
-}
+	public class Ex03 {
 
-public static void main(String[] args) {
-	new Ex03();
-}
+	public Ex03() {
+		
+		TreeSet set = new TreeSet();
+		
+		while(true) {
+			Student stud = new Student((int)(Math.random() * 9 + 1), (int)(Math.random() * 10 + 1),
+					(int)(Math.random() * 41 + 60), (int)(Math.random() * 41 + 60),
+					(int)(Math.random() * 41 + 60), (int)(Math.random() * 41 + 60));
+			
+			set.add(stud);
+			
+			if(set.size() == 10) {
+				break;
+			}
+		}
+			
+			// 출력
+			ArrayList list = new ArrayList(set);
+			for(Object o : list) {
+				System.out.println(o);
+			}
+		}
 
-}
+	public static void main(String[] args) {
+			new Ex03();
+		}
+	}
--- start/src/day15/Student.java
+++ start/src/day15/Student.java
@@ -1,117 +1,108 @@
 package day15;
 
-public class Student {
-	private String name, ban, tel;
-	private int java, db, web, spring, sum;
+public class Student implements Comparable {
+	private String name;
+	private int ban, num, java, db, web, spring, sum;
 	private double avg;
 	
 	public Student() {}
-	
-	public Student(String name, String ban, String tel) {
-		this.name = name;
+	public Student(int ban, int num, int java, int db, int web, int spring) {
 		this.ban = ban;
-		this.tel = tel;
-	}
-	
-	public Student(int java, int db, int web, int spring, int sum) {
+		this.num = num;
 		this.java = java;
 		this.db = db;
 		this.web = web;
 		this.spring = spring;
+	
+	}
+	
+	public Student(int sum) {
 		setSum();
 	}
 	
 	public Student(double avg) {
-		this.avg = avg;
 		setAvg();
 	}
 	
 	public void setSum() {
 		sum = java + db + web + spring;
-		
 	}
 	
 	public void setAvg() {
-		avg = (java + db + web + spring) / 4.0;
+		avg = sum / 4.0;
 	}
-
+	
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getBan() {
+	public int getBan() {
 		return ban;
 	}
-
-	public void setBan(String ban) {
+	public void setBan(int ban) {
 		this.ban = ban;
 	}
-
-	public String getTel() {
-		return tel;
+	public int getNum() {
+		return num;
 	}
-
-	public void setTel(String tel) {
-		this.tel = tel;
+	public void setNum(int num) {
+		this.num = num;
 	}
-
 	public int getJava() {
 		return java;
 	}
-
 	public void setJava(int java) {
 		this.java = java;
 	}
-
 	public int getDb() {
 		return db;
 	}
-
 	public void setDb(int db) {
 		this.db = db;
 	}
-
 	public int getWeb() {
 		return web;
 	}
-
 	public void setWeb(int web) {
 		this.web = web;
 	}
-
 	public int getSpring() {
 		return spring;
 	}
-
 	public void setSpring(int spring) {
 		this.spring = spring;
 	}
-
 	public int getSum() {
 		return sum;
 	}
-
 	public void setSum(int sum) {
 		this.sum = sum;
 	}
-
 	public double getAvg() {
 		return avg;
 	}
-
 	public void setAvg(double avg) {
 		this.avg = avg;
 	}
 	
 	@Override
-	public String toString() {
-		return "학생 - 이름 : " + name + " , 반 : " + ban + " , 번호 : " + tel +
-				" , 자바 : " + java + ", 데이터베이스 : " + db + " , 웹 : " + web + " , 스프링 : " + spring +
-				" , 총점 : " + sum  + " , 평균 : " + avg;
+	public int compareTo(Object o) {
+		// 나와 다른 Student를 비교하는 기능이므로
+		// 입력된 데이터를 Student 타입으로 강제 형변환
+		Student stud = (Student) o ;
+		
+		// 반을 기준으로 오름차순 정렬 예정
+		int result = ban - stud.getBan();
+		
+		return result;
 	}
-
+	
+	@Override
+	public String toString() {
+		return "학생  - 이름 : " + name + " , 반 : " + ban + " , 번호 : " + num
+				+ " , 자바 : " + java + " , 데이터베이스 : " + db + " , 웹 : " + web + " , 스프링 : " + spring
+				+ ", 총점 : " + sum + " , 평균 : " + avg;
+	}
 }
