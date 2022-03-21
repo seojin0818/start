723f937832cc5bbdf959b9954e58be013fe026b5
User <User@DESKTOP-RT0MR1A>
2022-03-21 23:50:53 +0900

    day16 commit

---
+++ start/src/day16/ex/Ex02.java
@@ -0,0 +1,24 @@
+package day16.ex;
+
+import java.util.*;
+
+public class Ex02 {
+	
+/*
+ 
+	학생들의 과목점수를 기억하는 HashMap을 만들고
+	다섯 학생의 점수를 저장해서 출력하는 프로그램을 작성하세요.
+	과목은
+		국어, 영어, 수학, 과학, 국사
+	의 점수를 기억하도록 하세요.
+	
+ */
+	
+	public Ex02() {
+	}
+
+	public static void main(String[] args) {
+		new Ex02();
+	}
+
+}
