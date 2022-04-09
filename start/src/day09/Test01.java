dafda7a343d6d2a5c7ef72a3d4c62d6ebe36a77b
User <User@DESKTOP-RT0MR1A>
2022-04-09 23:49:43 +0900

    day09 commit

--- start/src/day09/Test01.java
+++ start/src/day09/Test01.java
@@ -1,11 +1,11 @@
 package day09;
 
 public class Test01 {
+	
 	int no = 10;
 	
-	// abc라는 이름의 함수
 	static void abc(int no) {
-		
+	// abc라는 이름의 함수
 	}
 	
 	public static void main(String[] args) {
@@ -13,17 +13,22 @@ public class Test01 {
 		abc(10);
 	}
 
-	void xyz() { // 무언가 기능이 필요한 경우 함수 제작
+	void xyz() { 
+	// xyz라는 이름의 함수
+	// 무언가 기능이 필요한 경우 함수 제작
 		int no = 10;
 		abc(no);
 		
-		// 1, "숫자 : " ==> "숫자 : 1"
-//		add("abc : ", 10); // 순서가 달라서 안됨 매개변수의 형태가 달라지게 입력했기 때문에
+// 		1, "숫자 : " ==> "숫자 : 1"
+//		add("abc : ", 10); ==> 매개변수의 순서가 다르므로 X
 		add(10, "abc : ");
 	}
 	
 	public String add(int no, String str) {
-		String result = str + no; // 문자열 + 정수 = 문자열
+	// add라는 이름의 함수
+	// 반환값은 String 타입
+		String result = str + no; 
+		// 문자열 + 정수 = 문자열
 		return result;
 	}
 }
