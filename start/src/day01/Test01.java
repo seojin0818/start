c23d0dda75f11a6139e978a4b1a36af7af9fdc33
User <User@DESKTOP-RT0MR1A>
2022-04-09 14:04:14 +0900

    day01 commit

--- start/src/day01/Test01.java
+++ start/src/day01/Test01.java
@@ -1,12 +1,16 @@
 package day01;
 
 public class Test01 {
+	// 클래스의 접근지정자는 public 혹은 생략만 가능
+	// 클래스 이름의 첫 글자는 대문자
+	// 특수문자는 _, $ 두 가지만 가능 (첫 글자로도 가능)
+	// 두 단어 이상으로 구성될 때는 각 단어의 첫 글자를 대문자로 표기하는 Camel 표기법
 	public static void main(String[] args) {
 	// 진입점 함수
 		
 		System.out.println("첫번째 자바 클래스");
 		// System.out.println(내용);
-		// 내용 출력 + 줄 바꿈
+		// 내용 출력 + 줄 바꿈 o
 	}
 
 }
