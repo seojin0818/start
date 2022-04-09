a40f739963410fbcfd4aaacb707e8d21c4eccb50
User <User@DESKTOP-RT0MR1A>
2022-04-09 23:22:47 +0900

    day02 commit

--- start/src/day02/Test02.java
+++ start/src/day02/Test02.java
@@ -11,12 +11,12 @@ public class Test02 {
 		
 		// 문자열 변수를 만드는 방법
 		
-		// 1) 데이터를 직접 입력해서 만드는 방법
+		// 1. 데이터를 직접 입력해서 만드는 방법
 		String name1 = "홍길동";
-		// 2) 클래스를 new 시켜서 만드는 방법
+		// 2. 클래스를 new 시켜서 만드는 방법
 		String name2 = new String("홍길동");
 		
-		// 3) 최종 출력
+		// 최종 출력하기
 		System.out.println("name1 : " + name1);
 		System.out.println("name2 : " + name2);
 		System.out.println("name1 == name2 : " + (name1 == name2));
