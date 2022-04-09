da4d09c50f514bfeaac0ab250b8e4efae5afcd03
User <User@DESKTOP-RT0MR1A>
2022-04-09 15:00:56 +0900

    day02 commit

--- start/src/day02/Test02.java
+++ start/src/day02/Test02.java
@@ -1,6 +1,11 @@
 package day02;
 
 public class Test02 {
+	// 클래스의 접근지정자는 public 혹은 생략만 가능
+	// 클래스 이름의 첫 글자는 대문자
+	// 특수문자는 _, $ 두 가지만 가능 (첫 글자로도 가능)
+	// 두 단어 이상으로 구성될 때는 각 단어의 첫 글자를 대문자로 표기하는 Camel 표기법
+	
 	public static void main(String[] args) {
 	// 진입점 함수
 		
@@ -11,13 +16,14 @@ public class Test02 {
 		// 2) 클래스를 new 시켜서 만드는 방법
 		String name2 = new String("홍길동");
 		
+		// 3) 최종 출력
 		System.out.println("name1 : " + name1);
 		System.out.println("name2 : " + name2);
 		System.out.println("name1 == name2 : " + (name1 == name2));
 		//System.out.println(내용);
 		// 내용 출력 + 줄 바꿈
 		
-		// 클래스를 new 시키면 주소가 다르기 때문에 name1과 naem2는 다름
+		// 클래스를 new 시키면 주소가 다르기 때문에 name1과 name2는 같지 않음
 		
 	}	
 }
--- start/src/day02/Test04.java
+++ start/src/day02/Test04.java
@@ -1,41 +1,63 @@
 package day02;
 
 /**
- *  자신의 정보를 기억하고 출력하는 기능의 클래스
+ * 
+ * 자신의 정보를 기억하고 출력하는 기능의 클래스
  * @author 백서진
  * @since 2022.02.23
  * @version v.1.0
  * 
  */
+
+/*
+
+ 문제 ] 자신의 이름, 나이, 이름 첫 문자, 키를 변수에 기억시키고 출력하세요.
+ 
+*/
+
 public class Test04 {
-	/*
-	 
-	 	 문제 ] 자신의 이름, 나이, 이름 첫문자, 키를 변수에 기억시키고 출력하세요.
-	 	 
-	 */
+	// 클래스의 접근지정자는 public 혹은 생략만 가능
+	// 클래스 이름의 첫 글자는 대문자
+	// 특수문자는 _, $ 두 가지만 가능 (첫 글자로도 가능)
+	// 두 단어 이상으로 구성될 때는 각 단어의 첫 글자를 대문자로 표기하는 Camel 표기법
+	
 	public static void main(String[] args) {
-		// 진입점 함수
+	// 진입점 함수
+		
+		/*
 		
 		// 할 일
-		// 1) 이름, 나이, 이름 첫문자, 키의 데이터 타입 정하기
-		// 2) 이름은 문자열이므로 String, 나이는 숫자이므로 정수형 (기본 타입 int), 이름 첫문자는 문자이므로 char, 키는 숫자이므로 정수형도 되지만 실수형으로 만들어보기
-		// 3) 변수를 만들고 변수 초기화로 데이터 기억시키기
+		// 1) 데이터타입 정하기
+		String name;
+		int age;
+		char start;
+		double height;
+		// 이름은 문자열, 나이는 숫자, 이름 첫 문자는 문자, 키는 숫자이므로 정수형도 가능하지만 실수형으로 만들어보기
 		
+		// 2) 데이터 기억시키기
+		name = "백서진";
+		age = 27;
+		start = '백';
+		height = 165.; // double형은 소수점이 있는 실수형이므로 . 을 붙이기
+		
+		*/
+		
+		// 변수 선언 & 변수 초기화
 		String name = "백서진";
-		int age = 27;
-		char sijak = '백';
+		int age = 27; // 정수형 기본 타입 int
+		char start = '백';
 		double height = 165.;
-		// double형은 소수점이 있는 실수형에서 쓰기 때문에 . 을 붙여줌
 		
-		// 4) 최종 출력
+		// 최종 출력
 		System.out.println("이름 : " + name);
 		System.out.println("나이 : " + age);
-		System.out.println("첫문자 : " + sijak);
+		System.out.println("이름 첫 문자 : " + start);
 		System.out.println("키 : " + height);
 		// System.out.println(내용);
 		// 내용 출력 + 줄 바꿈
 		
 		System.out.println("'가' > '나' : " + ('가' > '나'));
+		// 문자는 정수를 사용하고 있으므로 대소비교 가능
 	}
 	
 }
